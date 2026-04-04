40f57dedeb883d8e1a3080e910e9a3ccbfffeec1
SebiTCR <Ssebi832@outlook.com>
2026-04-04 10:42:13 +0300

    Fixed cloning method

--- core/fs.py
+++ core/fs.py
@@ -10,9 +10,28 @@ def get_latest_tag(path: str):
     return sorted(repo.tags, key= lambda t: t.commit.committed_datetime)
 
 
-def clone_repo(url: str):
-    r = Repo(url)
-    r.clone(os.environ['REPOS_DIR'])
+def clone_repo(url: str) -> Repo:
+    """
+    Clones a repository and stores into [*REPO_PATH*]/repo_name
+    :param url: Git URL
+    :return: Repository object
+    """
+    name = url.split("/")[-1]
+    path = f'{os.environ["REPO_PATH"]}/{name}'
+
+    try:
+        r = Repo(path)
+    except InvalidGitRepositoryError:
+        log.debug(f"Clonning {repo}...")
+        r = Repo.clone_from(url, path)
+    else:
+        log.debug(f"Fetching {path}...")
+        r.remote().fetch()
+
+    r = Repo()
+    r.clone(url, path)
+    r = Repo(path)
+    return r
 
 
 def remove_project(id: int):
